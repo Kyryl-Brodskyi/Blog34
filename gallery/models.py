2ee29626a05e41e0a32fafb43d931280de625348
Kyryl-Brodskyi <158332194+Kyryl-Brodskyi@users.noreply.github.com>
2024-04-06 21:51:06 +0300

    Add files via upload

--- gallery/models.py
+++ gallery/models.py
@@ -2,8 +2,16 @@ from django.db import models
 
 
 class Photo(models.Model):
+    name = models.CharField(max_length=255, default="Default Name")
     image = models.ImageField(upload_to='photos/')
     description = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Фото"
+        verbose_name_plural = "Фото"
 
 
 class Post(models.Model):
